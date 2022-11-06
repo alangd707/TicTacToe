9228b6bd9970305edc6b6446bc16c5275b7ebe74
alangd707 <111930791+alangd707@users.noreply.github.com>
2022-11-05 18:18:27 -0700

    -Major features added
    -Added most functionality and created a semi-working game, some minor bugs must be resolved though alongside a overhaul to UI.

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
 'use strict';
-let button = (x) => { return document.getElementById(x)}
-let turn = true;
-let gameStatus = {
-    row1: [null, null, null],
-    row2: [null, null, null],
-    row3: [null, null, null]
-}
+let turn = true; // Starts at true since true represents O and O plays first.
+let oWins = 0;
+let xWins = 0;
+let rows = [
+    [null,null,null],
+    [null,null,null],
+    [null,null,null]
+]
 
-function changeValue(x, y) {
-    if (!button(x).innerHTML) {
-        if (turn) {
-            button(x).innerHTML = 'O';
-            turn = false;
-        } else {
-            button(x).innerHTML = 'X';
-            turn = true;
+ // Array that stores the buttons clicked and the player who clicked them, with true representing O and false representing X.
+let player = function() {return (turn) ? 'O' : 'X'}
+
+function buttonClick(position) { 
+    let id = (x) => { return (x[0] * 3) + (x[1] + 1) };
+    let relativeButton = document.getElementById(id(position));
+    if (!relativeButton.innerHTML) {
+        relativeButton.innerHTML = player()
+        rows[position[0]][position[1]] = turn;
+        checkGameStatus(turn)
+        turn = !turn;
+        document.getElementById('turn-display').innerHTML = player()
+    }
+} // Function that determines if the box hasn't been clicked on yet, and if so, set value of array that will be used further on with calculating if game has been decided. Changes all the visual aspects of the game as well.
+
+function checkGameStatus(Turn) {
+    for (let i = 0; i < 3; i++) {
+        let column = [rows[0][i], rows[1][i], rows[2][i]];
+        let diagonalOne = [rows[0][0], rows[1][1], rows[2][2]];
+        let diagonalTwo = [rows[2][0], rows[1][1], rows[0][2]];
+        let checkIfTrue = (x) => x.every((y) => y === Turn);
+        if (checkIfTrue(rows[i]) || checkIfTrue(column) || checkIfTrue(diagonalOne) || checkIfTrue(diagonalTwo)) {
+            gameWon();
+            break;
+        } else if (!rows[0].includes(null) && !rows[1].includes(null) && !rows[2].includes(null)) {
+            staleMate();
+            break;
         }
     }
+} // Function that executes each time a button is clicked to either continue the game or end the game resulting in a victory for either X or O, relative to the turn that the function is executed at. Changes all the 
+
+function gameWon() {
+    console.log(player() + ' has won!');
+    reset()
+} // Function that is executed if someone successfully wins the game.
+
+function staleMate() {
+    console.log('Nobody has won!')
+    reset()
+} // Function that is executed if there is no clear winner.
+
+function reset() {
+    let buttons = document.getElementsByClassName('button')
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].innerHTML = ''
+    }
+    for (let i = 0; i < 3; i++) {
+        rows[i] = [null, null, null];
+    }
+    turn = true;
+    console.log(turn)
 }
