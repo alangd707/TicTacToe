3dcf8f18df586cad313b971ad42d0a0ef086f468
alangd707 <111930791+alangd707@users.noreply.github.com>
2022-11-07 21:56:41 -0700

    Finished Game
    - Everything is totally functional, there might be a few bugs however for the most part game is final and is ready to be submited.

--- index.js
+++ index.js
@@ -1,60 +1,100 @@
 'use strict';
-let turn = true; // Starts at true since true represents O and O plays first.
-let rows = []
-for (let i = 0; i < 3; i++) {
-        rows[i] = [null, null, null];
-}
-
- // Array that stores the buttons clicked and the player who clicked them, with true representing O and false representing X.
-let player = () => turn ? 'O' : 'X'
-
-function buttonClick(position) { 
-    let truePlayer = turn;
-    let id = (x) => { return (x[0] * 3) + (x[1] + 1) };
-    let relativeButton = document.getElementById(id(position));
-    if (!relativeButton.innerHTML) {
-        relativeButton.innerHTML = player()
-        rows[position[0]][position[1]] = truePlayer;
-        checkGameStatus(turn)
-        document.getElementById('turn-display').innerHTML = player()
+let turn = true;
+let buttons = document.getElementsByClassName('button');
+let xWins = 0;
+let oWins = 0;
+let rows = [ 
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+];
+let findButtonPosition = (position) => buttons[(position[0] * 3) + position[1]]; // Function that converts the array data into the corresponding button to display game to user.
+let player = (x) => {
+    if (x === true) {
+        return 'O';
+    } else if (x === false) {
+        return 'X';
+    } else {
+        return null;
     }
-} // Function that determines if the box hasn't been clicked on yet, and if so, set value of array that will be used further on with calculating if game has been decided. Changes all the visual aspects of the game as well.
-
-function checkGameStatus() {
+} // Variables set at start of game alongside some variable functions to help shorten code further on.
+function tempReset(hard) {
+    turn = true;
+    rows = [ 
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+    ];
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].style.backgroundColor = 'rgb(240,240,240)';
+    }
+    updateInterface();
+    if (hard) {xWins = 0; oWins = 0;}
+} //Function that resets the state of the game, paramater hard determines if reset will be hard reset.
+function checkGameState() {
     for (let i = 0; i < 3; i++) {
         let column = [rows[0][i], rows[1][i], rows[2][i]];
         let diagonalOne = [rows[0][0], rows[1][1], rows[2][2]];
         let diagonalTwo = [rows[2][0], rows[1][1], rows[0][2]];
         let checkIfTrue = (x) => x.every((y) => y === turn);
-        if (checkIfTrue(rows[i]) || checkIfTrue(column) || checkIfTrue(diagonalOne) || checkIfTrue(diagonalTwo)) {
-            gameWon();
+        if (checkIfTrue(rows[i])) {
+            changeColor('row', i);
+        } else if (checkIfTrue(column)) {
+            changeColor('column', i);
             break;
-        } else if (rows.forEach(element => element.includes(null))) {
+        } else if (checkIfTrue(diagonalOne)) {
+            changeColor('diagonalOne', i);
+            break;
+        } else if (checkIfTrue(diagonalTwo)) {
+            changeColor('diagonalTwo', i);
+            break;
+        } else if (rows.every(element => !element.includes(null))){
             staleMate();
             break;
-        } else {
-            turn = !turn;
+        }
+    } // For loop for 3 times, i means the row/column it checks. Passes i down to checkIfTrue() if game has been won.
+    turn = !turn;
+} // Checks if a game has reached a point where someone has one or game is unwinnable.
+function changeColor(form, i) {
+    for (let a = 0; a < 3; a++) {
+        if (form === 'row') {
+            findButtonPosition([i, a]).style.backgroundColor = 'green';
+        } else if (form === 'column') {
+            findButtonPosition([a, i]).style.backgroundColor = 'green';
+        } else if (form === 'diagonalOne') {
+            findButtonPosition([0,0]).style.backgroundColor = 'green';
+            findButtonPosition([1,1]).style.backgroundColor = 'green';
+            findButtonPosition([2,2]).style.backgroundColor = 'green';
+        } else if (form === 'diagonalTwo') {
+            findButtonPosition([2,0]).style.backgroundColor = 'green';
+            findButtonPosition([1,1]).style.backgroundColor = 'green';
+            findButtonPosition([0,2]).style.backgroundColor = 'green';
+        }
+    } // For loop to 
+    gameWon();
+} // Changes the color of the winning blocks to the respective row/column.  
+function gameWon() {
+    alert(player(turn) + ' has won!');
+    if (turn) {oWins++} else {xWins++};
+    updateInterface();
+} // Function that is called if game is won
+function staleMate() {
+    alert('Nobody has won!');
+} // Function that is called if stalemate is reached.
+function updateInterface() {
+    for (let i = 0; i < 3; i++) {
+        for (let a = 0; a < 3; a++) {
+            findButtonPosition([i, a]).innerHTML = player(rows[i][a]);
         }
     }
-} // Function that executes each time a button is clicked to either continue the game or end the game resulting in a victory for either X or O, relative to the turn that the function is executed at. Changes all the 
-
-function gameWon() {
-    console.log(player() + ' has won!');
-    reset()
-} // Function that is executed if someone successfully wins the game.
-
-function staleMate() {
-    console.log('Nobody has won!')
-    reset()
-} // Function that is executed if there is no clear winner.
-
-function reset() {
-    let buttons = document.getElementsByClassName('button')
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].innerHTML = ''
+    document.getElementById('turn-display').innerHTML = player(turn);
+    document.getElementById('o-wins').innerHTML = oWins;
+    document.getElementById('x-wins').innerHTML = xWins;
+} // Updates the text to display the game's 
+function buttonClick(position) {
+    if (!findButtonPosition(position).innerHTML) {
+        rows[position[0]][position[1]] = turn;
+        checkGameState();
+        updateInterface();
     }
-    for (let i = 0; i < 3; i++) {
-        rows[i] = [null, null, null];
-    }
-    turn = true;
-}
+} // Updates the multi dimensional array from moves taken to be further used to decide game status. Each time button is clicked, game state is checked.
